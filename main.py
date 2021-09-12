c449d4c7e6f9ff775a711d4198b5218f45b037ae
cecigolazo <84542560+cecigolazo@users.noreply.github.com>
2021-09-12 19:46:08 -0300

    commit 6

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def division(a,b):
 
 if __name__ == "main":
    print("Principal")
-   print("La resta es:  ", str(suma(125,25)))
+   print("La resta es:  ", str(resta(125,25)))
    print("La suma es:  ", str(suma(6,9)))
    print("La division es:  ", str(division(6,9)))
