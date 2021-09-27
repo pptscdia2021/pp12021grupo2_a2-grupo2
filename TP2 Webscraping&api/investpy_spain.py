3ffa795120e99e3184412e2692f1e03e62402df7
cecigolazo <84542560+cecigolazo@users.noreply.github.com>
2021-09-27 14:17:54 -0300

    commit 33

--- TP2 Webscraping&api/investpy_spain.py
+++ TP2 Webscraping&api/investpy_spain.py
@@ -1,10 +1,16 @@
 import investpy
 import pandas as pd
 
+
+#Obtener la tabla en tiempo real de la bolsa de españa
+
+def obtenerDatosInvestpy():
+   return investpy.stocks.get_stocks_overview(country='spain', as_json = False , n_results = 35)
+        
+df = obtenerDatosInvestpy()
+
+#Guardar datos en csv
 def cargarTablaEnCsv():
-   #Obtener la tabla en tiempo real de la bolsa de españa
-   df = investpy.stocks.get_stocks_overview(country='spain', as_json = False , n_results = 35)
+  df.to_csv("TP2 Webscraping&api\\investpy_spain.csv")
 
-   #Guardar datps en csv
-   df.to_csv("TP2 Webscraping&api\\investpy_spain.csv")
-
+#guardar = cargarTablaEnCsv()
