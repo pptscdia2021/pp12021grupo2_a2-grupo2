9f616fbf444e12d9f926fec8b3458fdfe961857e
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2021-09-28 23:08:42 -0300

    Mostrar tablas

--- TP2 Webscraping&api/BolsaMadrid.py
+++ TP2 Webscraping&api/BolsaMadrid.py
@@ -1,42 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-#Hacemos la consulta a la pagina y guardamos el contenido en una variable soup
-url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
-page = requests.get(url_page).text 
-soup = BeautifulSoup(page, "lxml")
+def obtenerDatos():
+    #Hacemos la consulta a la pagina y guardamos el contenido en una variable soup
+    url_page = 'https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000'
+    page = requests.get(url_page).text 
+    soup = BeautifulSoup(page, "lxml")
 
-#Buscamos la tabla de donde obtener los datos
-tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblÍndices'})
+    #Buscamos la tabla de donde obtener los datos
+    tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblAcciones'})
 
-#creamos una lista vacia que llenaremos con los datos de las tablas
-columnas = []
+    #creamos una lista vacia que llenaremos con los datos de las tablas
+    columnas = []
 
-#buscamos los encabezados de la tabla y los agregamos a la lista columnos (deberia ubicarse en la posicion 0)
-header = []
-for th in tabla.find_all('th'):
-    header.append(th.text)
-columnas.append(header)
+    #buscamos los encabezados de la tabla y los agregamos a la lista columnos (deberia ubicarse en la posicion 0)
+    header = []
+    for th in tabla.find_all('th'):
+        header.append(th.text)
+    columnas.append(header)
 
-for tr in tabla.find_all('tr'):
-    fila = []
-    for td in tr.find_all('td'):
-        fila.append(td.text)
-    if len(fila) > 0:
-        columnas.append(fila)
-        
-#creamos el dataframe usando la lista columnas,pasamos el contenido (de la posicion 1 en adelante), y lo encabezados en (posicion 0)
-df = pd.DataFrame(columnas[1:], columns=columnas[0])
+    for tr in tabla.find_all('tr'):
+        fila = []
+        for td in tr.find_all('td'):
+            fila.append(td.text)
+        if len(fila) > 0:
+            columnas.append(fila)
+            
+    #creamos el dataframe usando la lista columnas,pasamos el contenido (de la posicion 1 en adelante), y lo encabezados en (posicion 0)
+    df = pd.DataFrame(columnas[1:], columns=columnas[0])
 
-#Guardamos en el archivo csv
+    #Guardamos en el archivo csv
 
-df.to_csv('csv\\bolsaMadrid.csv')
+    df.to_csv('TP2 Webscraping&api\\csv\\bolsaMadrid.csv')
 
 
--- TP2 Webscraping&api/main.py
+++ TP2 Webscraping&api/main.py
@@ -1,5 +1,6 @@
 import investpy_spain as invest
 import yahoofinance
+import BolsaMadrid
 import pandas as pd
 
 def obtenerGananciaPerdida(df, columna, cant):
@@ -10,15 +11,32 @@ def obtenerGananciaPerdida(df, columna, cant):
 if __name__ == "__main__":
    #Obtener la tabla en tiempo real de la bolsa de españa
    invest.obtenerDatosInvestpy()
-
+   BolsaMadrid.obtenerDatos()
+   
+   #Mostrar tabla en pantalla usando investpy
+   print('TABLA DE DATOS BOLSA DE MADRID (USANDO investpy)')
    df_investpy = pd.read_csv('TP2 Webscraping&api\\csv\\investpy_spain.csv')
    accionesAevaluar = ['ANA', 'BBVA', 'GRLS', 'ELE', 'REP']
    print(df_investpy[df_investpy.symbol.isin(accionesAevaluar)])
 
+   #Mostrar tabla en pantalla usando BeautiFoul Soup
+   print('')
+   print('TABLA DE DATOS BOLSA DE MADRID (USANDO BS4)')
+   df_bolsaMadrid = pd.read_csv('TP2 Webscraping&api\\csv\\bolsaMadrid.csv')
+   accionesAevaluar = ['ACCIONA', 'BBVA', 'GRIFOLS CL.A', 'ENDESA', 'REPSOL']
+   print(df_bolsaMadrid[df_bolsaMadrid.Nombre.isin(accionesAevaluar)])
+
+   #Mostrar tabla en pantalla usando yfinance (bolsa eeuu)
+   print('')
+   print('TABLA DE DATOS EEUU (USANDO YFINANCE)')
    accionesAevaluar = ["ORI", "ANA.MC", "BBVA.MC", "GRF.MC", "ELE.MC"]
    yahoofinance.obtenerDatos(accionesAevaluar)
    df_yfinance = pd.read_csv('TP2 Webscraping&api\\csv\\yahoo_finance.csv')
    print (df_yfinance)
 
    #Objetivo 2
+   print('2 ACCIONES DE MAYOR GANANCIA, MAYOR PERDIDA - INVESTPY')
    obtenerGananciaPerdida(df_investpy, 'G/P', 2)
+   print('')
+   print('2 ACCIONES DE MAYOR GANANCIA, MAYOR PERDIDA - BOLSA DE MADRID')
+   obtenerGananciaPerdida(df_bolsaMadrid, '% Dif.', 2)
