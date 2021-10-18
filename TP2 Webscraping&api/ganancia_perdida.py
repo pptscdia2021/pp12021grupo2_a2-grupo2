65a221e5f5e665d2a427e3f94730626b4a795463
cecigolazo <84542560+cecigolazo@users.noreply.github.com>
2021-10-18 12:38:25 -0300

    commit 48

--- TP2 Webscraping&api/ganancia_perdida.py
+++ TP2 Webscraping&api/ganancia_perdida.py
@@ -3,13 +3,37 @@ import matplotlib.pyplot as plt
 
 
 def obtenerGananciaPerdida(df, columna, cant):
+  #ordeno las columnas según el % de ganancia o pérdida de forma descendente          
   df = df.sort_values(by=columna, ascending = False)
   print("Acciones de mayor ganancia\n",df.head(cant))
   print("Acciones de mayor pérdida\n",df.tail(cant))
 
-def graficarGananciaPerdida(df,x,y,kind,titulo,nombre):
-    ax = df.plot(x,y,kind);print(ax)
+
+def graficarGananciaPerdida(df,x,y,titulo,nombre):
+    #vamos a graficar los valores positivos de un color y los negativos de otro color
+    #seleccionamos los valores positivos
+    dfMas=df[y]>=0
+    dfPositivos = df[dfMas]
+    #renombramos la columna como Ganancia
+    dfPositivos = dfPositivos.rename({y: 'Ganancia'}, axis=1)
+    #seleccionamos los valores negativos
+    dfMenos=df[y]<0
+    dfNegativos = df[dfMenos]
+    #renombramos la columna como Pérdida
+    dfNegativos = dfNegativos.rename({y: 'Pérdida'}, axis=1)
+    #unimos las tablas ordenadas por el nombre
+    df = pd.concat([dfPositivos,dfNegativos]).sort_values(by=x)
+
+    #armamos el gráfico 
+    #parámetros: columnas, tipo de gráfico, ancho de las barras, colores de las barras
+    df[[x, 'Ganancia', 'Pérdida']].plot(x, kind="bar", width=1, color=["#04D8B2","#EF4026"])
+    #título del grafico
     plt.title(titulo)
+    #título del eje de las x
+    plt.xlabel('Acciones')
+    #guardamos el gráfico, 'tight' s para que se guarde el gráfico de manera completa
+    #parámetro: nombre
     plt.savefig(nombre, bbox_inches='tight')
+    #mostramos el gráfico de manera completa
     plt.tight_layout();plt.show()
     
--- TP2 Webscraping&api/graficos.py
+++ TP2 Webscraping&api/graficos.py
@@ -4,20 +4,21 @@ import matplotlib.pyplot as plt
 
 
 def graficarMaxMin(df,x,max,min,titulo,nombre):
+    plt.style.use('Solarize_Light2')
     fig, ax = plt.subplots()
-    fig.set_size_inches(7,12)
+    #fig.set_size_inches(7,12)
     x = df[x]
     max = df[max]
     min = df[min]
-    ax.plot(x, max, label ='Valor máximo')
-    ax.plot(x, min, label ='Valor mínimo', linestyle = 'dashed')
+    ax.plot(x, max, label ='Valor máximo', color="#04D8B2")
+    ax.plot(x, min, label ='Valor mínimo', linestyle = 'dashed', color="#FA8072")
     ax.set_title(titulo, loc = "left", fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:blue'})
     plt.legend() 
     plt.savefig(nombre, bbox_inches='tight')
     plt.tight_layout();plt.show()
 
 def graficarComparacion(df,x,titulo,nombre):
-    plt.style.use('ggplot')
+    plt.style.use('seaborn')
     df.set_index(x).plot.bar(rot=0, title=titulo, figsize=(6,6), fontsize=12)
     plt.savefig(nombre, bbox_inches='tight')
     plt.tight_layout();plt.show()
