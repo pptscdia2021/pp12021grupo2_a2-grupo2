9afdf714894a3455905f2da0234cb20a05c03024
cecigolazo <84542560+cecigolazo@users.noreply.github.com>
2021-11-06 12:06:02 -0300

    correcciones varias

--- TP2 Webscraping&api/ganancia_perdida.py
+++ TP2 Webscraping&api/ganancia_perdida.py
@@ -11,29 +11,13 @@ def obtenerGananciaPerdida(df, columna, cant):
 
 def graficarGananciaPerdida(df,x,y,titulo,nombre):
     #vamos a graficar los valores positivos de un color y los negativos de otro color
-    #seleccionamos los valores positivos
-    dfMas=df[y]>=0
-    dfPositivos = df[dfMas]
-    #renombramos la columna como Ganancia
-    dfPositivos = dfPositivos.rename({y: 'Ganancia'}, axis=1)
-    #seleccionamos los valores negativos
-    dfMenos=df[y]<0
-    dfNegativos = df[dfMenos]
-    #renombramos la columna como Pérdida
-    dfNegativos = dfNegativos.rename({y: 'Pérdida'}, axis=1)
-    #unimos las tablas ordenadas por el nombre
-    df = pd.concat([dfPositivos,dfNegativos]).sort_values(by=x)
-
+    dfPositivos = df[df[y]>=0].rename({y: 'Ganancia'}, axis=1)#filtramos las acciones con ganancia
+    dfNegativos = df[df[y]<0 ].rename({y: 'Pérdida'}, axis=1)#filtramos las acciones con pérdida
+    df = pd.concat([dfPositivos,dfNegativos]).sort_values(by=x)   #unimos las tablas ordenadas por el nombre
     #armamos el gráfico 
-    #parámetros: columnas, tipo de gráfico, ancho de las barras, colores de las barras
-    df[[x, 'Ganancia', 'Pérdida']].plot(x, kind="bar", width=1, color=["#04D8B2","#EF4026"])
-    #título del grafico
-    plt.title(titulo)
-    #título del eje de las x
-    plt.xlabel('Acciones')
-    #guardamos el gráfico, 'tight' s para que se guarde el gráfico de manera completa
-    #parámetro: nombre
-    plt.savefig(nombre, bbox_inches='tight')
-    #mostramos el gráfico de manera completa
-    plt.tight_layout();plt.show()
-    
+    plt.style.use('seaborn') #estilo grafico
+    df[[x, 'Ganancia', 'Pérdida']].plot(x, kind="bar", width=1, color=["#04D8B2","#EF4026"]) #parámetros: columnas, tipo de gráfico, ancho de las barras, colores de las barras
+    plt.title(titulo) #título del grafico
+    plt.xlabel('Acciones') #título del eje de las x
+    plt.savefig(nombre, bbox_inches='tight') #guardamos el gráfico, 'tight' para guardarlo completo [parámetro: nombre]
+    plt.tight_layout();plt.show() #mostramos el gráfico de manera completa
